38618aec8414674d01c889a682222c6cf3f423de
axexlck <axelclk@gmail.com>
2020-02-04 22:39:10 +0100

    use mathcell@1.8.7

--- symja_web/src/main/java/org/matheclipse/gwt/server/AJAXQueryServlet.java
+++ symja_web/src/main/java/org/matheclipse/gwt/server/AJAXQueryServlet.java
@@ -114,7 +114,7 @@ public class AJAXQueryServlet extends HttpServlet {
 					"<body style=\"width: 100%; height: 100%; margin: 0; padding: 0\">\n" + //
 					"\n" + //
 					"<script src=\"https://cdn.jsdelivr.net/gh/paulmasson/math@1.2.6/build/math.js\"></script>\n" + //
-					"<script src=\"https://cdn.jsdelivr.net/gh/paulmasson/mathcell@1.8.6/build/mathcell.js\"></script>\n"
+					"<script src=\"https://cdn.jsdelivr.net/gh/paulmasson/mathcell@1.8.7/build/mathcell.js\"></script>\n"
 					+ //
 					"<script src=\"https://cdn.jsdelivr.net/gh/mathjax/MathJax@2.7.5/MathJax.js?config=TeX-AMS_HTML\"></script>"
 					+ //
